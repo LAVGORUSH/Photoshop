454aa0039030af87ca22589b7b74af8f664b3656
lavgorush <lavgorush@gmail.com>
2020-01-08 13:33:11 +0400

    Add GrayFilter

---
+++ Filters/GrayFilter.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace MyPhotoshop
+{
+	public class GrayFilter : IFilter
+	{
+		public ParameterInfo[] GetParameters()
+		{
+			return new []
+			{
+				new ParameterInfo { Name="Коэффициент", MaxValue=10, MinValue=0, Increment=0.1, DefaultValue=1 }
+				
+			};
+		}
+		
+		public override string ToString ()
+		{
+			return "Отенки серого";
+		}
+		
+		public Photo Process(Photo original, double[] parameters)
+		{
+			var result=new Photo(original.width, original.height);
+			for (int x = 0; x < result.width; x++)
+				for (int y = 0; y < result.height; y++)
+				{
+					result[x, y] = new Pixel(original[x, y].R, original[x, y].R, original[x, y].R) * parameters[0];
+				}
+			return result;
+		}
+	}
+}
+
