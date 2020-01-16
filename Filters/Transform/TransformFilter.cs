9c2536bfa4078da6615fc5a1a6eb2be4c2bbf416
lavgorush <lavgorush@gmail.com>
2020-01-17 00:20:36 +0400

    Refract TransformFilter to generic class and new Filter

--- Filters/Transform/TransformFilter.cs
+++ Filters/Transform/TransformFilter.cs
@@ -6,31 +6,35 @@ using System.Text;
 
 namespace MyPhotoshop
 {
-    public class TransformFilter : ParametrizedFilter<EmptyParameters>
+    public class TransformFilter<TParameters> : ParametrizedFilter<TParameters>
+        where TParameters : IParameters, new()
     {
         public string Name { get; set; }
-        Func<Size, Size> sizeTransform;
-        Func<Point, Size, Point> pointTransform;
+        Func<Size, TParameters, Size> sizeTransform;
+        Func<Size, Point, TParameters, Point?> pointTransform;
 
-        public TransformFilter(string name, Func<Size, Size> sizeTransform, Func<Point, Size, Point> pointTransform)
+        public TransformFilter(string name, Func<Size, TParameters, Size> sizeTransform, Func<Size, Point, TParameters, Point?> pointTransform)
         {
             Name = name;
             this.sizeTransform = sizeTransform;
             this.pointTransform = pointTransform;
         }
 
-        public override Photo Process(Photo original, EmptyParameters parameters)
+        public override Photo Process(Photo original, TParameters parameters)
         {
             var oldSize = new Size(original.width, original.height);
-            var newSize = sizeTransform(oldSize);
+            var newSize = sizeTransform(oldSize, parameters);
             var result = new Photo(newSize.Width, newSize.Height);
             for (int x = 0; x < newSize.Width; x++)
             {
                 for (int y = 0; y < newSize.Height; y++)
                 {
                     var point = new Point(x, y);
-                    var oldPoint = pointTransform(point, oldSize);
-                    result[x, y] = original[oldPoint.X,oldPoint.Y];
+                    var oldPoint = pointTransform(oldSize, point, parameters);
+                    if (oldPoint.HasValue)
+                    {
+                        result[x, y] = original[oldPoint.Value.X, oldPoint.Value.Y];
+                    }
                 }
             }
             return result;
